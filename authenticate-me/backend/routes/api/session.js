4c6c77f20614e27fe9f2a48360501ebe6a54111a
Efrain <efrain.p.saldana@gmail.com>
2022-10-21 18:08:00 -0500

    Add Login route

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,5 +1,24 @@
+// Import required files / packages
 const express = require("express");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
 const router = express.Router();
 
+router.post("/", async (req, res, next) => {
+    const { credential, password } = req.body;
+    const user = await User.login({ credential, password });
 
-module.exports = route;
+    if (!user) {
+        const err = new Error("Login failed");
+        err.status = 401;
+        err.title = "Login failed";
+        err.errors = ["the provided credentials were invalid"];
+        return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+});
+
+module.exports = router;
