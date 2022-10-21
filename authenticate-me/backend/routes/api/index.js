4f9055792681355e664e1761ed42031dbc591979
Efrain <efrain.p.saldana@gmail.com>
2022-10-20 23:30:12 -0500

    Remove testing routes

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -6,24 +6,5 @@ const { restoreUser } = require("../../utils/auth.js");
 const { requireAuth } = require("../../utils/auth.js");
 // Test Routes
 router.use(restoreUser);
-router.post("/test", (req, res, next) => {
-    res.json({ requestBody: req.body });
-});
-
-router.get("/set-token-cookie", async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: "Demo-lition"
-        }
-    });
-
-    setTokenCookie(res, user);
-
-    return res.json({ user });
-});
-
-router.get("/restore-user", (req, res) => res.json(req.user));
-
-router.get("/require-auth", requireAuth, (req, res) => res.json(req.user));
 
 module.exports = router;
