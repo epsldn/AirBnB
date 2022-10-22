b09cac6e8ad7471a3fadcf12ce630fa3d2eaa13a
Efrain <efrain.p.saldana@gmail.com>
2022-10-21 23:51:45 -0500

    Add GET session route

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -4,6 +4,15 @@ const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
 
+router.get("/", restoreUser, (req, res, next) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    } else return res.json({});
+});
+
 router.post("/", async (req, res, next) => {
     const { credential, password } = req.body;
     const user = await User.login({ credential, password });
