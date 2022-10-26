ddb4261a701ffb7e83df02d7bc319be6cac02b34
Efrain <efrain.p.saldana@gmail.com>
2022-10-26 15:40:58 -0500

    Complete 'Get details of a post from an id'

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,12 +1,68 @@
 const express = require("express");
 const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
-const { Spot, SpotImage, Review, Sequelize, sequelize } = require("../../db/models");
+const { Spot, SpotImage, Review, Sequelize, sequelize, User } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
 router.get("");
 
+router.get("/current", requireAuth, async (req, res, next) => {
+    const ownerId = req.user.id;
+
+    let spot = await Spot.findAll({
+        where: {
+            ownerId
+        },
+        include: [{
+            model: SpotImage,
+            attributes: [],
+            where: { preview: true },
+            order: ["preview"],
+            required: false
+        }, {
+            model: Review,
+            attributes: []
+        }],
+        attributes: ["id", "ownerId", "address", "city", "state", "country", "lat", "lng", "name", "description", "price", "createdAt", "updatedAt", [Sequelize.fn("avg", sequelize.col("Reviews.stars")), "avgRating"], [sequelize.col("SpotImages.url"), "previewImage"]],
+        group: ["Spot.id", ["SpotImages.id"]]
+    });
+
+    res.json({ Spots: spot });
+
+});
+
+router.get("/:spotId", async (req, res, next) => {
+    const id = req.params.spotId;
+    const spot = await Spot.findByPk(id, {
+        include: [
+            {
+                model: SpotImage,
+                attributes: {
+                    exclude: ["spotId", "createdAt", "updatedAt"]
+                }
+            }, {
+                model: User,
+                as: "Owner"
+            },
+            {
+                model: Review,
+                attributes: []
+            }],
+        attributes: {
+            include: [[Sequelize.fn("COUNT", sequelize.col("Reviews.id")), "numReviews"]]
+        },
+        group: ["SpotImages.id"]
+    });
+
+    if (!spot) {
+        const err = new Error();
+        err.message = "Spot couldn't be found";
+        err.status = 404;
+        return next(err);
+    };
+    res.json(spot);
+});
 router.get("/", async (req, res, next) => {
     let spot = await Spot.findAll({
         include: [{
