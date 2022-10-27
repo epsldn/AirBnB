1c70c2ece33c17f7533233e04e273f3beeac8998
Efrain <efrain.p.saldana@gmail.com>
2022-10-27 12:10:20 -0500

    Add new bookings router and connect to app

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,13 +8,14 @@ const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewRouter = require("./reviews.js");
-
+const bookingsRouter = require("./bookings.js");
 // Test Routes
 router.use(restoreUser);
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 router.use("/spots", spotsRouter);
 router.use("/reviews", reviewRouter);
+router.use("/bookings", bookingsRouter);
 router.get("/test", (req, res, next) => {
     res.json({ requestBody: req.body });
 });
