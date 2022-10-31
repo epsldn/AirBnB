c5940bab8bfc0173e83d038a46399dd003e75b0d
Efrain <efrain.p.saldana@gmail.com>
2022-10-30 23:34:44 -0500

    Match error from docs

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -30,11 +30,16 @@ const validateReview = [
     handleValidationErrors
 ];
 
+// const validateBooking = [
+//     check("startDate", "Start date cannot be in the past").custom((value, { req }) => Date.parse(value) >= Date.parse(new Date().toDateString())),
+//     check("startDate", "Please provide a start date.").exists({ checkFalsy: true }),
+//     check("endDate", "endDate cannot be on or before startDate").custom((value, { req }) => { return Date.parse(value) > Date.parse(req.body.startDate); }),
+//     check("endDate", "Please provide an end date.").exists({ checkFalsy: true }),
+//     handleValidationErrors
+// ];
+
 const validateBooking = [
-    check("startDate", "Start date cannot be in the past").custom((value, { req }) => Date.parse(value) >= Date.parse(new Date().toDateString())),
-    check("startDate", "Please provide a start date.").exists({ checkFalsy: true }),
-    check("endDate", "endDate cannot be on or before startDate").custom((value, { req }) => { return Date.parse(value) > Date.parse(req.body.startDate); }),
-    check("endDate", "Please provide an end date.").exists({ checkFalsy: true }),
+    check("endDate", "endDate cannot be on or before startDate").exists({ checkFalsy: true }).custom((value, { req }) => { return Date.parse(value) > Date.parse(req.body.startDate); }),
     handleValidationErrors
 ];
 
