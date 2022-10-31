7346af90dce3990ff092fd34b78ac8f7ff84229f
Efrain <efrain.p.saldana@gmail.com>
2022-10-30 23:18:59 -0500

    Fix response for edit by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -370,7 +370,7 @@ router.put("/:spotId", requireAuth, validateSpot, async (req, res, next) => {
     });
 
     await spot.save();
-    return res.json({spot});
+    return res.json(spot);
 });
 
 router.delete("/:spotId", requireAuth, async (req, res, next) => {
