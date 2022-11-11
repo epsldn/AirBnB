bb7e7d63fb12c0d3d3df272ef333b1753ec71d31
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-11 17:17:43 -0600

    Test addUser to store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from "./store";
 import { csrfFetch, restoreCSRF } from './store/csrf';
-
+import * as sessionReducer from './store/sessionReducer';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
+  window.sessionReducer = sessionReducer;
   window.csrfFetch = csrfFetch;
   window.store = store;
 };
---
+++ frontend/src/store/sessionReducer.js
@@ -0,0 +1,51 @@
+import { csrfFetch } from "./csrf";
+
+
+const ADD_USER = "session/addUser";
+const REMOVE_USER = "session/removeUser";
+
+const addUser = (user) => {
+    return {
+        type: ADD_USER,
+        user
+    };
+};
+
+const removeUser = (userId) => {
+    return {
+        action: REMOVE_USER,
+        userId
+    };
+};
+
+export const getUserFromDb = (credential, password) => async dispatch => {
+    const response = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({ credential, password })
+    });
+
+    if (response.ok) {
+        const user = await response.json();
+        console.log(user)
+        dispatch(addUser(user));
+        return user;
+    } else {
+        throw response;
+    }
+};
+
+export default (state = { user: null }, action) => {
+    switch (action.type) {
+        case ADD_USER: {
+            return action.user;
+        }
+        case REMOVE_USER: {
+            return {
+                user: null
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
