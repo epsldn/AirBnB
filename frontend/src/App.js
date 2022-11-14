b58db786ea12adcb80a58963fd5504d6783a0b5b
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-13 21:17:25 -0600

    Persist user session in store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./component/LoginFormPage/LoginFormPage";
+import * as sessionReducer from "./store/sessionReducer";
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionReducer.getSession()).then(_ => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
