528e42a9f1a648efda3840164c8aafaa3a631c06
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-15 13:01:48 -0600

    Create thunk action to fetch spots

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import spotReducer from "./spots";
 
 const rootReducer = combineReducers({
-    session: sessionReducer
+    session: sessionReducer,
+    spots: spotReducer
 });
 
 let enhancer;
