95f7a79580749221abd2582fd80d5853e3df258b
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-17 08:38:39 -0600

    Compllete demo css for create/update spot

--- frontend/src/components/SpotForm/SpotForms.js
+++ frontend/src/components/SpotForm/SpotForms.js
@@ -67,51 +67,53 @@ export default function SpotForm() {
 
     return (
         <div id="spot-form-outer-container">
-            <h1 id="spot-form-title">{inCreateSpot ? "Let's get you started!" : "Let's update your spot!"}</h1>
-            <form onSubmit={onSubmit} id="spot-form-container" className="form">
-                {hasSubmitted && <ul className="errors">
-                    {errors.map((error, idx) => <li key={idx}><i className="fa-solid fa-circle-exclamation"></i>{" " + error}</li>)}
-                </ul>}
-                <input
-                    type="text"
-                    onChange={event => setName(event.target.value)}
-                    value={name}
-                    placeholder="Name"
-                    className="form-first"
-                />
-                <input
-                    type="text"
-                    onChange={event => setAddress(event.target.value)}
-                    value={address}
-                    placeholder="Street Address"
-                />
-                <input
-                    type="text"
-                    onChange={event => setCity(event.target.value)}
-                    value={city}
-                    placeholder="City"
-                />
-                <input
-                    type="text"
-                    onChange={event => setCountry(event.target.value)}
-                    value={country}
-                    placeholder="Country"
-                />
-                <textarea
-                    onChange={event => setDescription(event.target.value)}
-                    value={description}
-                    placeholder="Tell us about your place"
-                    id="spot-form-textarea"
-                />
-                <input
-                    type="number"
-                    onChange={event => setPrice(event.target.value)}
-                    value={price}
-                    placeholder="Price per night"
-                    className="form-last"
-                />
-                <button type="submit" id="submit">{inCreateSpot ? "Create" : "Update"} Spot!</button>
-            </form>
+            <div>
+                <h1 id="spot-form-title">{inCreateSpot ? "Let's get you started!" : "Let's update your spot!"}</h1>
+                <form onSubmit={onSubmit} id="spot-form-container" className="form">
+                    {hasSubmitted && <ul className="errors" id="spot-form-errors">
+                        {errors.map((error, idx) => <li key={idx}><i className="fa-solid fa-circle-exclamation"></i>{" " + error}</li>)}
+                    </ul>}
+                    <input
+                        type="text"
+                        onChange={event => setName(event.target.value)}
+                        value={name}
+                        placeholder="Name"
+                        className="form-first"
+                    />
+                    <input
+                        type="text"
+                        onChange={event => setAddress(event.target.value)}
+                        value={address}
+                        placeholder="Street Address"
+                    />
+                    <input
+                        type="text"
+                        onChange={event => setCity(event.target.value)}
+                        value={city}
+                        placeholder="City"
+                    />
+                    <input
+                        type="text"
+                        onChange={event => setCountry(event.target.value)}
+                        value={country}
+                        placeholder="Country"
+                    />
+                    <textarea
+                        onChange={event => setDescription(event.target.value)}
+                        value={description}
+                        placeholder="Tell us about your place"
+                        id="spot-form-textarea"
+                    />
+                    <input
+                        type="number"
+                        onChange={event => setPrice(event.target.value)}
+                        value={price}
+                        placeholder="Price per night"
+                        className="form-last"
+                    />
+                    <button type="submit" id="submit">{inCreateSpot ? "Create" : "Update"} Spot!</button>
+                </form>
+            </div>
         </div>
     );
 }
