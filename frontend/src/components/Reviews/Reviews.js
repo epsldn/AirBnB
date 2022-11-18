2504aa0b166364279142e1813246772e16f24b68
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-18 10:44:24 -0600

    add review skeleton

--- frontend/src/components/Reviews/Reviews.js
+++ frontend/src/components/Reviews/Reviews.js
@@ -6,6 +6,8 @@ import "./Reviews.css";
 export default function Reviews({ spot }) {
     const dispatch = useDispatch();
     const reviews = useSelector(state => state.reviews.Reviews);
+    const user = useSelector(state => state.session.user);
+
     useEffect(() => {
         dispatch(getReviews(spot.id));
     }, [dispatch]);
@@ -20,9 +22,26 @@ export default function Reviews({ spot }) {
                 <p id="spot-reviews-num-reviews">{`${spot.numReviews} `}<span>reviews</span></p>
             </div>
             <div>
-                <ul>
-                    {reviews.map(review => <li key={review.id}>{review.review}</li>)}
-                </ul>
+                {reviews.map(review => (
+                    <div key={review.id}>
+                        <div>
+                            <div>
+                                <i className="fas fa-user-circle" />
+                            </div>
+                            <div>
+                                <p>{review.User.firstName}</p>
+                                <p>{new Date(review.createdAt).toLocaleString("default", { month: "long" })}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>{review.review}</p>
+                        </div>
+                        {review.User.id === user.id && <div>
+                            <button className="demo-button">Edit</button>
+                            <button className="demo-button">Delete</button>
+                        </div>}
+                    </div>
+                ))}
             </div>
         </div>
     );
