3aa2595e5d3f752904e9cc5dabd90fd0cbebd5ac
Efrain Saldana <efrain.p.saldana@gmail.com>
2022-11-19 00:25:45 -0600

    correct grid on chrome

--- frontend/src/components/AllSpots/AllSpots.js
+++ frontend/src/components/AllSpots/AllSpots.js
@@ -21,21 +21,19 @@ export default function AllSpots() {
             <ul className="all-spots-gallery">
                 {Object.values(spots).map(spot => (
                     <Link to={{ pathname: `/spots/${spot.id}`, state: spots }} key={spot.id} className="spots-outer-frame">
-                        <div>
-                            <div className="spots-gallery-image-container">
-                                <img alt="Property" src={spot.previewImage ?? "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"} />
+                        <div className="spots-gallery-image-container">
+                            <img alt="Property" src={spot.previewImage ?? "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"} />
+                        </div>
+                        <div className="spots-information-container">
+                            <div className="spots-information">
+                                <ul>
+                                    <li id="spot-address">{`${spot.city}, ${spot.state}`}</li>
+                                    <li id="spot-host">{`Hosted by ${spot.ownerName[0].toUpperCase() + spot.ownerName.slice(1).toLowerCase()}`}</li>
+                                    <li id="spot-price"><span>{"$" + usDollar.format(Number(spot.price))}</span> {`night`}</li>
+                                </ul>
                             </div>
-                            <div className="spots-information-container">
-                                <div className="spots-information">
-                                    <ul>
-                                        <li id="spot-address">{`${spot.city}, ${spot.state}`}</li>
-                                        <li id="spot-host">{`Hosted by ${spot.ownerName[0].toUpperCase() + spot.ownerName.slice(1).toLowerCase()}`}</li>
-                                        <li id="spot-price"><span>{"$" + usDollar.format(Number(spot.price))}</span> {`night`}</li>
-                                    </ul>
-                                </div>
-                                <div className="spots-rating">
-                                    <i className="fa-solid fa-star"></i> <p>{`${spot.avgRating ??= "New"}`}</p>
-                                </div>
+                            <div className="spots-rating">
+                                <i className="fa-solid fa-star"></i> <p>{`${spot.avgRating ??= "New"}`}</p>
                             </div>
                         </div>
                     </Link>
